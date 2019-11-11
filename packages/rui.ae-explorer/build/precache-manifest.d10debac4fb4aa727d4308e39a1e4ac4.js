21a87ed713ed721a63873fa47123beda9f8b5bd8
fsl0110 <lechner.frank@web.de>
2019-11-11 01:14:56 +0100

    add build folders to git

---
+++ packages/rui.ae-explorer/build/precache-manifest.d10debac4fb4aa727d4308e39a1e4ac4.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/2.66cbcfde7e258711b549.css"
+  },
+  {
+    "revision": "23e958ff7e293dbc4127b6fe976a30ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cbcfde7e258711b549",
+    "url": "/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "66cbcfde7e258711b549",
+    "url": "/static/js/2.d1013fd1.chunk.js"
+  },
+  {
+    "revision": "766d197f84849d7e58af",
+    "url": "/static/js/main.31475d28.chunk.js"
+  },
+  {
+    "revision": "e9a60c0b3a56fb54d987",
+    "url": "/static/js/runtime-main.c8cb6c52.js"
+  }
+]);
